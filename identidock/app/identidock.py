24ada52d407740ed033c54d83a8112a70ea20372
ueriku <ue.riku@gmail.com>
2017-02-25 22:45:02 +0900

    91pまで実施

---
+++ identidock/app/identidock.py
@@ -0,0 +1,30 @@
+from flask import Flask, Response
+import requests
+
+app = Flask(__name__)
+default_name = 'Joe Bloggs'
+
+@app.route('/')
+def manipage():
+    name = default_name
+    header = '<html><head><title>Identidock</title></head><body>'
+    body = '''<form method="POST">
+              Hello <input type="text" name="name" value="{}">
+              <input type="submit" value="submit">
+              </form>
+              <p>You look like a:
+              <img src="/monster/monster.png" />
+              '''.format(name)
+    footer = '</body></html>'
+
+    return header + body + footer
+
+@app.route('/monter/<name>')
+def get_identicon(name):
+    r = requests.get('http://dnmonster:8080/monster/' + name + 'size=80')
+    image = r.content
+
+    return Response(image, mimeType='image/png')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
