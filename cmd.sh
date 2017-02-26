1f3d2630b1849170991f235eb0b27d3a867d66c0
ueriku <ue.riku@gmail.com>
2017-02-26 16:55:25 +0900

    124pまで実行

--- cmd.sh
+++ cmd.sh
@@ -4,6 +4,9 @@ set -e
 if [ "$ENV" = 'DEV' ]; then
   echo "Running Development Server"
   exec python "identidock.py"
+elif [ "$ENV" = 'UNIT' ]; then
+  echo "Running Unit Tests"
+  exec python "tests.py"
 else
   echo "Running Production Server"
   exec python "identidock.py"
